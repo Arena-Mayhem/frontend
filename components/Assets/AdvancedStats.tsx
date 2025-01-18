5eb6f5a5e1934e526382d3f33ac57406bbf661e6
Andrea Feliciani <afelicia@student.42madrid.com>
2025-01-18 22:03:31 +0100

    fix: mobile view for p&l fixed

--- components/Assets/AdvancedStats.tsx
+++ components/Assets/AdvancedStats.tsx
@@ -5,8 +5,8 @@ export default function AdvancedStats() {
   const wins = 10;
   const games = 20;
   return (
-    <div className="bg-arena-bg p-8 m-8 border border-b-[0.1px] border-white/20 rounded-lg w-full shadow-padentro  ">
-      <div className="div-oblicuo p-8 bg-arena-black  gradient-border relative border-b-2">
+    <div className=" lg:w-full overflow-x-hidden md:bg-arena-bg lg:p-8 lg:m-8 lg:border lg:border-b md:shadow-padentrobg-arena-bg bg-cover p-0 sm:p-8 md:border md:border-b mx-auto sm:m-8 md:border-white/20 md:rounded-lg sm:max-w-6xl w-[calc(100%-2rem)] md:shadow-padentro mb-2 mt-4">
+      <div className="div-oblicuo p-8 bg-arena-black gradient-border relative border-b-2">
         <img
           src="/square.svg"
           className="absolute top-0 left-0 pointer-events-none"
--- components/Assets/Stats.tsx
+++ components/Assets/Stats.tsx
@@ -25,31 +25,46 @@ export default function Stats({
 
   return (
     <>
-      <div className="">
-        <div className="flex items-center justify-between">
-          <h1 className="text-2xl text-white">ADVANCED STATS</h1>
+      <div className="md:overflow-x-auto">
+      <div className="md:min-w-[400px]"> 
+        <div className="flex items-center justify-between md:pt-2 lg:py-2 w-full md:px-4">
+        <h1 className="text-xl md:hidden text-white">
+  ADVANCED<br />STATS
+</h1>
+          <h1 className="hidden md:block text-4xl text-white">ADVANCED STATS</h1>
+          
           <div
             aria-note-dev="container wins and games"
-            className="flex justify-between  gap-4 items-center"
+            className="flex flex-col xl:flex-row justify-between gap-4 items-center "
           >
-            <div className="flex items-center gap-2 px-8 border-r-[1px]">
-              <Image src="/ap.svg" alt="assets" width={35} height={35} />
-              <p className="text-white">Games: {games}</p>
+            <div className="flex items-center gap-2 md:px-8 xl:border-r">
+            <Image 
+  src="/ap.svg" 
+  alt="assets" 
+  width={23} 
+  height={23}
+  className="md:w-[35px] md:h-[35px] pb-1"
+/>
+              <p className="text-white text-xs md:text-base ">games: {games}</p>
             </div>
-            <div className="flex items-center gap-2 px-4">
-              <IconCup />
-              <p className="text-white">Wins: {wins}</p>
+            
+            <div className="flex items-center gap-2 md:px-4">
+            <div className="w-[23px] h-[23px] md:w-[35px] md:h-[35px]">
+  <IconCup />
+</div>
+              <p className="text-white text-xs md:text-base">wins: {wins}</p>
             </div>
           </div>
         </div>
-        <div className="border-t-arena-orange border-t-[1px] mt-8"></div>
-        <div className="flex flex-row gap-4 pt-8">
+        <div className="border-t-arena-orange border-t mt-8 md:mt-4 hidden lg:block"></div>
+        <div className="flex flex-col xl:flex-row gap-4 pt-4 md:pt-12 -ml-3 -mr-3 md:-ml-0 md:-mr-0">
           <Profits data={profitsData} />
           <Losses data={profitsData} />
         </div>
         <div className=" w-full flex items-center justify-end pt-4">
           <ShareTwitter />
         </div>
+      </div>
       </div>
     </>
   );
